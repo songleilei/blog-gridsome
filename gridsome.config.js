d5fed8f8fba98d3b05b158fb710a358750206fd8
lei.song <moon.song@yeahmobi.com>
2020-09-19 15:21:46 +0800

    feat: 从 strapi 获取数据

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,15 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+    siteName: 'Gridsome',
+    plugins: [
+        {
+            use: '@gridsome/source-strapi',
+            options: {
+                apiURL: 'http://localhost:1337',
+                queryLimit: 1000, // Defaults to 100
+                contentTypes: ['post'],
+            },
+        },
+    ],
 }
